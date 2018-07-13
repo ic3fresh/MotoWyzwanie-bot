a39eb099583ddc6b1d0fc6cbb67d9d65a6190be0
Gerard Kowalski <zgderek92@gmail.com>
2018-07-13 11:43:05 +0200

    add some lists for operations
    Signed-off-by: Gerard Kowalski <zgderek92@gmail.com>

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ DEBUG = False
 
 # API_TOKEN = os.environ["API_TOKEN"]
 API_URL = "http://127.0.0.1:4567/api/"  # "https://paciak.pl/api/"
-API_SLEEP = 1
 TOKEN = {"Authorization": "Bearer ciach"}
 score_page_url = 'topic/44/punktacja'
+category_page_url = 'category/7/motowyzwanie?page={0}'
 
 
 # TOKEN = {"Authorization": "Bearer {API_TOKEN}".format(API_TOKEN=API_TOKEN)}
@@ -41,68 +41,78 @@ def call_to_api(req):
     return response
 
 
-def open_score_page():
-    return json.loads(call_to_api(score_page_url).text)
+def get_score_page():
+    return json.loads(call_to_api(score_page_url).text)["posts"][0]['content'].split('\n')
 
 
-def extract_score_content():
-    return open_score_page()["posts"][0]['content']
+def get_category_page(current_page_number):
+    return json.loads(call_to_api(category_page_url.format(current_page_number)).text)
 
 
-def get_user_name(data):
-    return re.search('(@[^<]+)', data)
-
-
-def get_user_points(data):
-    return re.search('(\ \d+)', data)
-
-
-def get_challenge_number(data):
-    return re.search('(MotoWyzwanie.*\#[^<])', data)
-
-
-def get_challenge_link(data):
-    return re.search('(http[^\"]+)', data)
-
-
-def divide_by_lines():
-    return extract_score_content().split('\n')
-
+USERNAME_REGEXP = re.compile('@[^<]+')
+USERPOINTS_REGEXP = re.compile('\ \d+')
+CHALLENGE_NUMBER_REGEXP = re.compile('MotoWyzwanie.*\#[^<]+')
+CHALLENGE_LINK_REGEXP = re.compile('http[^\"]+')
+TOPIC_NUMBER_REGEXP = re.compile('\d+')
 
 points = []
+finished_challenges = []
+topics_list = []
+new_challenges_list = topics_list
 
 
 def create_actual_points_list():
-    for line in divide_by_lines():
+    for line in get_score_page():
         if line == '</ol>':
             break
-        if not get_user_points(line):
+        if not USERPOINTS_REGEXP.search(line):
             pass
         else:
-            points.append([get_user_name(line).group(0), get_user_points(line).group(0).replace(" ", "")])
-
-
-finished_challenges = []
+            points.append(
+                [USERNAME_REGEXP.search(line).group(0), USERPOINTS_REGEXP.search(line).group(0).replace(" ", "")])
 
 
 def create_finished_challenges_list():
-    for line in divide_by_lines():
-        if not get_challenge_number(line):
+    for line in get_score_page():
+        if not CHALLENGE_NUMBER_REGEXP.search(line):
             pass
         if 'MotoWyzwanie' not in line:
             pass
         else:
             finished_challenges.append(
-                [get_challenge_number(line).group(0), get_challenge_link(line).group(0), get_user_name(line).group(0)])
+                [CHALLENGE_NUMBER_REGEXP.search(line).group(0), CHALLENGE_LINK_REGEXP.search(line).group(0),
+                 USERNAME_REGEXP.search(line).group(0)])
+
+
+def create_topics_list():
+    active_page = 1
+    page_count = get_category_page(active_page)["pagination"]["pageCount"]
+    while active_page <= page_count:
+        for topic in get_category_page(active_page)["topics"]:
+            if not TOPIC_NUMBER_REGEXP.search(topic["title"]):
+                pass
+            else:
+                topics_list.append(topic["title"])
+        active_page += 1
+
+
+def check_for_new_challenges():
+    for element in finished_challenges:
+        for topic in topics_list:
+            if TOPIC_NUMBER_REGEXP.search(topic):
+                topic_number = int(TOPIC_NUMBER_REGEXP.search(topic).group(0))
+            else:
+                pass
+            if topic_number == int(TOPIC_NUMBER_REGEXP.search(element[0]).group(0)):
+                new_challenges_list.remove(topic)
+
 
 
 def main():
-    create_actual_points_list()
     create_finished_challenges_list()
-    for line in divide_by_lines():
-        print(line)
-    print(points)
-    print(finished_challenges)
+    create_topics_list()
+    check_for_new_challenges()
+    print(new_challenges_list)
 
 
 if __name__ == "__main__":
